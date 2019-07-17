a38d0adaa9fc7680268ca6d3a5c0780f3d3806a1
f1na1ik <maxsim119@yandex.ru>
2019-07-17 21:44:11 +0300

    slider 99% ready (сделать чтоб изначально нельзя было влево + в конце нельзя листать)

--- projects/layout2/js/main.js
+++ projects/layout2/js/main.js
@@ -1,4 +1,7 @@
-#(function () {
+$(function () {
 
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-next slick-button"></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-button"></button>'
+    })
 });
-
