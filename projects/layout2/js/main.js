71a4d466af34218d67368258e0317f31ac0d0b28
f1na1ik <maxsim119@yandex.ru>
2019-07-19 19:38:36 +0300

    block 'consultation' ready, +jquery form styler (select)

--- projects/layout2/js/main.js
+++ projects/layout2/js/main.js
@@ -3,5 +3,7 @@ $(function () {
     $('.slider__inner').slick({
         nextArrow: '<button type="button" class="slick-next slick-button"></button>',
         prevArrow: '<button type="button" class="slick-prev slick-button"></button>'
-    })
+    });
+
+    $('select').styler();
 });
