244b5af7a536e051f3a504b3e289130d53709a05
f1na1ik <maxsim119@yandex.ru>
2019-07-20 22:47:19 +0300

    block 'news' ready

--- projects/layout2/js/main.js
+++ projects/layout2/js/main.js
@@ -1,9 +1,11 @@
 $(function () {
 
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__slider-inner').slick({
         nextArrow: '<button type="button" class="slick-next slick-button"></button>',
         prevArrow: '<button type="button" class="slick-prev slick-button"></button>'
     });
 
     $('select').styler();
+
 });
+
