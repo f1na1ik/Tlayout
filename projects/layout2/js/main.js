d5a4abe3a23a8fa71b7d1013cff4e84461dff7e2
f1na1ik <maxsim119@yandex.ru>
2019-07-24 23:04:01 +0300

    adaptiv 99% (доделать футер, размеры карты разобраться)

--- projects/layout2/js/main.js
+++ projects/layout2/js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
     $('.slider__inner, .news__slider-inner').slick({
         nextArrow: '<button type="button" class="slick-next slick-button"></button>',
-        prevArrow: '<button type="button" class="slick-prev slick-button"></button>'
+        prevArrow: '<button type="button" class="slick-prev slick-button"></button>',
     });
 
     $('select').styler();
